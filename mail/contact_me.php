f2b43ce14f3266e27f1b6664b14a5a082e9b4088
Osman <ozuberi@umich.edu>
2018-06-01 16:11:57 -0400

    fixed php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,6 +16,8 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+$time = strip_tags(htmlspecialchars($_POST['time']));
+$service = strip_tags(htmlspecialchars($_POST['service']));
    
 // Create the email and send the message
 $to = 'bookings@thetalormadeagency.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
